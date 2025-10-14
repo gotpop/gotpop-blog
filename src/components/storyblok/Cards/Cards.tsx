86d242a651cd2c1ac47e5d5d44138a6d9e5e8035
GotPop <gotpop@users.noreply.github.com>
2025-10-14 19:25:51 +0100

    Adding style tag

--- src/components/storyblok/Cards/Cards.tsx
+++ src/components/storyblok/Cards/Cards.tsx
@@ -1,5 +1,6 @@
 import { CardsStoryblok } from "@/types/storyblok-components"
 import { StoryblokServerComponent } from "@/components/StoryblokServerComponent"
+import { getInlineStyles } from "@/utils/inline-styles"
 import { storyblokEditable } from "@storyblok/react/rsc"
 
 interface CardsProps {
@@ -7,8 +8,11 @@ interface CardsProps {
 }
 
 export default function Cards({ blok }: CardsProps) {
+  const styles = getInlineStyles("Cards.css")
+
   return (
     <div {...storyblokEditable(blok)} className="cards-grid">
+      {styles && <style>{styles}</style>}
       {blok.cards?.map((nestedBlok) => (
         <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
       ))}
