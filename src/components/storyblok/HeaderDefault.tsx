61e175f90bad57f73ec3b65f65f2f824f265d8ae
GotPop <gotpop@users.noreply.github.com>
2025-10-14 14:24:41 +0100

    Formatting

--- src/components/storyblok/HeaderDefault.tsx
+++ src/components/storyblok/HeaderDefault.tsx
@@ -7,18 +7,14 @@ interface HeaderDefaultProps {
 }
 
 export default function HeaderDefault({ blok }: HeaderDefaultProps) {
-  // Handle case where blok is null
   if (!blok) {
     return null;
   }
-
-  console.log("Header content:", JSON.stringify(blok, null, 2));
 
   const { nav, logo } = blok;
 
   return (
     <header {...storyblokEditable(blok)} className="header">
-      <h1>Liam Header</h1>
       {logo && logo[0] && <StoryblokServerComponent blok={logo[0]} />}
       {nav && nav[0] && <StoryblokServerComponent blok={nav[0]} />}
     </header>
