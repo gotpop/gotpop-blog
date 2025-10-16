5e49be9e9aeeeed775ef87753b49fa9e039f30e6
GotPop <gotpop@users.noreply.github.com>
2025-10-16 16:58:37 +0100

    Consolidating configs in package.json

--- storyblok.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  token: process.env.STORYBLOK_ACCESS_TOKEN,
-  space: "auto", // Will be detected automatically
-  componentsPath: "src/types/storyblok-components.ts",
-  componentsDirectory: "src/components/storyblok",
-}
