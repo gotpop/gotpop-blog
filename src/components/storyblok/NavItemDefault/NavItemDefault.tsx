7ee9b4d4721989b31fdcb376944c41a5048ed37a
GotPop <gotpop@users.noreply.github.com>
2025-10-20 12:39:09 +0100

    Removing sb editable

--- src/components/storyblok/NavItemDefault/NavItemDefault.tsx
+++ src/components/storyblok/NavItemDefault/NavItemDefault.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link"
 import type { NavItemDefaultStoryblok } from "@/types/storyblok-components"
-import { storyblokEditable } from "@storyblok/react/rsc"
 import { getStoryblokLinkProps } from "@/utils/storyblok"
 
 interface NavItemDefaultProps {
@@ -11,11 +10,7 @@ export default function NavItemDefault({ blok }: NavItemDefaultProps) {
   const linkProps = getStoryblokLinkProps(blok.link)
 
   if (!blok.link || linkProps.href === "#") {
-    return (
-      <span {...storyblokEditable(blok)} className="nav-item">
-        {blok.text}
-      </span>
-    )
+    return <span className="nav-item">{blok.text}</span>
   }
 
   return (
@@ -23,7 +18,6 @@ export default function NavItemDefault({ blok }: NavItemDefaultProps) {
       href={linkProps.href}
       target={linkProps.target}
       rel={linkProps.rel}
-      {...storyblokEditable(blok)}
       className="nav-item"
     >
       {blok.text}
