f0cbf8c0c7059803e7a3124c817b50ae9099d9e9
GotPop <gotpop@users.noreply.github.com>
2025-11-01 21:04:01 +0000

    Icon safari

--- src/components/storyblok/LogoDefault/LogoDefault.tsx
+++ src/components/storyblok/LogoDefault/LogoDefault.tsx
@@ -20,16 +20,74 @@ export default function LogoDefault({ blok }: LogoDefaultProps) {
         rel={linkProps.rel}
         target={linkProps.target}
       >
-        <IconLogo />
-        <svg className="svg" width="45" height="45" viewBox="0 0 100 100">
+        {/* <IconLogo /> */}
+        <svg
+          className="svg morphing-logo"
+          width="45"
+          height="45"
+          viewBox="0 0 100 100"
+        >
           <title>GotPop Logo</title>
-          <circle cx="75" cy="50" r="25" fill="oklch(1 0.4 100 / 0.5)" />
-          <circle cx="62.5" cy="71.65" r="25" fill="oklch(1 0.4 150 / 0.5)" />
-          <circle cx="37.5" cy="71.65" r="25" fill="oklch(1 0.4 200 / 0.5)" />
-
-          <circle cx="25" cy="50" r="25" fill="oklch(1 0.4 250 / 0.5)" />
-          <circle cx="37.5" cy="28.35" r="25" fill="oklch(1 0.4 300 / 0.5)" />
-          <circle cx="62.5" cy="28.35" r="25" fill="oklch(1 0.4 350 / 0.5)" />
+          <rect
+            className="morph-shape shape-1"
+            x="50"
+            y="25"
+            width="50"
+            height="50"
+            rx="25"
+            ry="25"
+            fill="oklch(1 0.4 100 / 0.5)"
+          />
+          <rect
+            className="morph-shape shape-2"
+            x="37.5"
+            y="46.65"
+            width="50"
+            height="50"
+            rx="25"
+            ry="25"
+            fill="oklch(1 0.4 150 / 0.5)"
+          />
+          <rect
+            className="morph-shape shape-3"
+            x="12.5"
+            y="46.65"
+            width="50"
+            height="50"
+            rx="25"
+            ry="25"
+            fill="oklch(1 0.4 200 / 0.5)"
+          />
+          <rect
+            className="morph-shape shape-4"
+            x="0"
+            y="25"
+            width="50"
+            height="50"
+            rx="25"
+            ry="25"
+            fill="oklch(1 0.4 250 / 0.5)"
+          />
+          <rect
+            className="morph-shape shape-5"
+            x="12.5"
+            y="3.35"
+            width="50"
+            height="50"
+            rx="25"
+            ry="25"
+            fill="oklch(1 0.4 300 / 0.5)"
+          />
+          <rect
+            className="morph-shape shape-6"
+            x="37.5"
+            y="3.35"
+            width="50"
+            height="50"
+            rx="25"
+            ry="25"
+            fill="oklch(1 0.4 350 / 0.5)"
+          />
         </svg>
         <span className="logo-text">GotPop</span>
       </a>
