d301085a0beebbd57d561dc13d39caa42a89e6d8
GotPop <gotpop@users.noreply.github.com>
2025-11-01 19:54:30 +0000

    Icon safari

--- src/components/storyblok/LogoDefault/LogoDefault.tsx
+++ src/components/storyblok/LogoDefault/LogoDefault.tsx
@@ -20,10 +20,18 @@ export default function LogoDefault({ blok }: LogoDefaultProps) {
         rel={linkProps.rel}
         target={linkProps.target}
       >
-        {/* <div className="logo" role="img" aria-label="GotPop Logo"> */}
         <IconLogo />
+        <svg className="svg" width="45" height="45" viewBox="0 0 100 100">
+          <title>GotPop Logo</title>
+          <circle cx="75" cy="50" r="25" fill="oklch(1 0.4 100 / 0.5)" />
+          <circle cx="62.5" cy="71.65" r="25" fill="oklch(1 0.4 150 / 0.5)" />
+          <circle cx="37.5" cy="71.65" r="25" fill="oklch(1 0.4 200 / 0.5)" />
+
+          <circle cx="25" cy="50" r="25" fill="oklch(1 0.4 250 / 0.5)" />
+          <circle cx="37.5" cy="28.35" r="25" fill="oklch(1 0.4 300 / 0.5)" />
+          <circle cx="62.5" cy="28.35" r="25" fill="oklch(1 0.4 350 / 0.5)" />
+        </svg>
         <span className="logo-text">GotPop</span>
-        {/* </div> */}
       </a>
     </logo-main>
   )
