97fe3f1bc6adfbdb1a2d5363433d1f71fc754dc5
GotPop <gotpop@users.noreply.github.com>
2025-11-08 16:15:50 +0000

    Updating system

--- src/components/storyblok/FilterContent/ClientSidePostCard.tsx
+++ src/components/storyblok/FilterContent/ClientSidePostCard.tsx
@@ -1,7 +1,7 @@
+import { Typography } from "@gotpop/system"
 import { getStoryPath } from "@/lib/storyblok-utils"
 import { formatDate } from "@/utils/date-formatter"
 import type { PostStory } from "@/utils/tags"
-import { Typography } from "../Typography"
 import "./ClientSidePostCard.css"
 
 interface PostCardProps {
