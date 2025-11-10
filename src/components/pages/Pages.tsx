ac7d06e38da25be4302e14c5d3e3118978911588
GotPop <gotpop@users.noreply.github.com>
2025-11-10 17:50:45 +0000

    Encapsulate logic in blocks

--- src/components/pages/Pages.tsx
+++ src/components/pages/Pages.tsx
@@ -1,5 +1,4 @@
-import { PageLayout, PostHeader } from "@gotpop/system"
-import type { ReactNode } from "react"
+import { CustomElement, PageLayout, PostHeader } from "@gotpop/system"
 import { StoryblokServerComponent } from "@/components"
 import { getStoryblokData } from "@/lib/storyblok"
 import type {
@@ -8,33 +7,22 @@ import type {
   PageDefaultStoryblok,
   PageFilterStoryblok,
   PagePostStoryblok,
-  StoryblokComponent,
 } from "@/types/storyblok-components"
 
-interface BasePageProps {
-  header?: string
-  footer?: string
-  body?: StoryblokComponent[]
-  children?: ReactNode
-}
+export async function PageDefault({ blok }: { blok: PageDefaultStoryblok }) {
+  const { Header = "", Footer = "", body } = blok
 
-async function BasePage({
-  header = "",
-  footer = "",
-  body,
-  children,
-}: BasePageProps) {
   const { data: headerData } = await getStoryblokData<HeaderDefaultStoryblok>(
     "storyByUuid",
     {
-      uuid: header,
+      uuid: Header,
     }
   )
 
   const { data: footerData } = await getStoryblokData<FooterDefaultStoryblok>(
     "storyByUuid",
     {
-      uuid: footer,
+      uuid: Footer,
     }
   )
 
@@ -44,41 +32,75 @@ async function BasePage({
 
   return (
     <PageLayout header={headerData.content} footer={footerData.content}>
-      {children}
       {blocks}
     </PageLayout>
   )
 }
 
-export async function PageDefault({ blok }: { blok: PageDefaultStoryblok }) {
-  const { Header, Footer, body } = blok
-
-  return <BasePage header={Header} footer={Footer} body={body} />
-}
-
 export async function PageFilter({ blok }: { blok: PageFilterStoryblok }) {
-  const { Header, Footer, body } = blok
+  const { Header = "", Footer = "", body } = blok
 
-  return <BasePage header={Header} footer={Footer} body={body} />
+  const { data: headerData } = await getStoryblokData<HeaderDefaultStoryblok>(
+    "storyByUuid",
+    {
+      uuid: Header,
+    }
+  )
+
+  const { data: footerData } = await getStoryblokData<FooterDefaultStoryblok>(
+    "storyByUuid",
+    {
+      uuid: Footer,
+    }
+  )
+
+  const blocks = body?.map((nestedBlok) => (
+    <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
+  ))
+
+  return (
+    <PageLayout header={headerData.content} footer={footerData.content}>
+      {blocks}
+    </PageLayout>
+  )
 }
 
 export async function PagePost({ blok }: { blok: PagePostStoryblok }) {
   const {
-    Header,
-    Footer,
+    Header = "",
+    Footer = "",
     Heading,
     published_date,
     body,
     view_transition_name,
   } = blok
 
+  const { data: headerData } = await getStoryblokData<HeaderDefaultStoryblok>(
+    "storyByUuid",
+    {
+      uuid: Header,
+    }
+  )
+
+  const { data: footerData } = await getStoryblokData<FooterDefaultStoryblok>(
+    "storyByUuid",
+    {
+      uuid: Footer,
+    }
+  )
+
+  const blocks = body?.map((nestedBlok) => (
+    <StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
+  ))
+
   return (
-    <BasePage header={Header} footer={Footer} body={body}>
+    <PageLayout header={headerData.content} footer={footerData.content}>
       <PostHeader
         heading={Heading}
         publishedDate={published_date}
         style={{ viewTransitionName: view_transition_name }}
       />
-    </BasePage>
+      <CustomElement tag="main-content">{blocks}</CustomElement>
+    </PageLayout>
   )
 }
